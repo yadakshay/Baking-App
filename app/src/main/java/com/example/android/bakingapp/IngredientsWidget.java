fdba47e2fdf6053ff76ea5e5f156a304d0bf3535
Akshay <yadakshay@gmail.com>
2017-12-04 11:10:44 +0530

    Implimented Stacked Widget

--- app/src/main/java/com/example/android/bakingapp/IngredientsWidget.java
+++ app/src/main/java/com/example/android/bakingapp/IngredientsWidget.java
@@ -3,12 +3,8 @@ package com.example.android.bakingapp;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.widget.RemoteViews;
-
-import com.example.android.bakingapp.customObjects.RecipeObject;
-
-import java.util.ArrayList;
 
 /**
  * Implementation of App Widget functionality.
@@ -20,19 +16,26 @@ public class IngredientsWidget extends AppWidgetProvider {
 
       //  CharSequence widgetText = context.getString(R.string.app_name);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget);
+
       //  views.setTextViewText(R.id.appwidget_text, widgetText);
-        new LoadRecipesForWidget(views, appWidgetManager, appWidgetId).execute();
+      //  new LoadRecipesForWidget(views, appWidgetManager, appWidgetId).execute();
         // Instruct the widget manager to update the widget
       //  appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        for (int i = 0; i < appWidgetIds.length; ++i) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget);
+            Intent serviceIntent = new Intent(context, StackWidgetServce.class);
+            views.setRemoteAdapter(appWidgetIds[i], R.id.stackWidgetView, serviceIntent);
+            views.setEmptyView(R.id.stackWidgetView, R.id.stackWidgetEmptyView);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], views);
         }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
@@ -43,29 +46,6 @@ public class IngredientsWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
-    }
-
-    public class LoadRecipesForWidget extends AsyncTask<Void, Void, ArrayList<RecipeObject>>{
-        private RemoteViews rv;
-        private AppWidgetManager wManager;
-        private int widgetId;
-        public LoadRecipesForWidget(RemoteViews views, AppWidgetManager awm, int wId){
-            rv = views;
-            wManager = awm;
-            widgetId = wId;
-        }
-        @Override
-        protected ArrayList<RecipeObject> doInBackground(Void... params) {
-            return NetworkUtils.getRecipes();
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<RecipeObject> recipeObjects) {
-           // super.onPostExecute(recipeObjects);
-            String s = recipeObjects.get(0).getIngredientList().get(0).getIngredient();
-            rv.setTextViewText(R.id.appwidget_text, s);
-            wManager.updateAppWidget(widgetId, rv);
-        }
     }
 }
 
--- app/src/main/java/com/example/android/bakingapp/StackWidgetServce.java
+++ app/src/main/java/com/example/android/bakingapp/StackWidgetServce.java
@@ -1,16 +1,90 @@
 package com.example.android.bakingapp;
 
-import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
 
-public class StackWidgetServce extends Service {
+import com.example.android.bakingapp.customObjects.IngredientsObject;
+import com.example.android.bakingapp.customObjects.RecipeObject;
+
+import java.util.ArrayList;
+
+public class StackWidgetServce extends RemoteViewsService {
+    private ArrayList<RecipeObject> mRecipes;
+
     public StackWidgetServce() {
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StackRemoteViewsFactory(this.getApplicationContext(), intent);
+    }
+
+
+    private class StackRemoteViewsFactory implements RemoteViewsFactory {
+        private Context mContext;
+        public StackRemoteViewsFactory(Context applicationContext, Intent intent) {
+            mContext = applicationContext;
+        }
+
+        @Override
+        public void onCreate() {
+
+        }
+
+        @Override
+        public void onDataSetChanged() {
+            mRecipes = NetworkUtils.getRecipes();
+        }
+
+        @Override
+        public void onDestroy() {
+
+        }
+
+        @Override
+        public int getCount() {
+            return mRecipes.size();
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_stack_item);
+            if (position <= getCount()) {
+                RecipeObject ro = mRecipes.get(position);
+                if(ro != null){
+                    rv.setTextViewText(R.id.header,ro.getRecipeName());
+                    ArrayList<IngredientsObject> mIngredientsList = ro.getIngredientList();
+                    String s = "";
+                    for (int i=0; i<mIngredientsList.size(); i++){
+                        IngredientsObject iOb = mIngredientsList.get(i);
+                        s = s + iOb.getIngredient() + "\t" + iOb.getQuantity()+ " " + iOb.getMeasure() + "\n";
+                    }
+                    rv.setTextViewText(R.id.ingredients, s);
+                }
+            }
+            return rv;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
     }
 }
